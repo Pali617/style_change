4e937a4dc6fa83735f00c896aabad0fd61b70adf
Pali <mrbadass617@gmail.com>
2018-01-15 16:00:40 +0100

    Raw Version

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Éjszakai Weblap</title>
+        <link rel="stylesheet" href="light.css">
+    </head>
+    <body>
+        <p>fejlesztés alatt</p>
+        <h2>Üdv a színváltás világában</h2>
+        <p> Vestibulum lobortis erat diam, vitae interdum turpis lobortis vitae. Donec facilisis faucibus aliquet. Vestibulum quis velit eget odio hendrerit imperdiet. Sed et porta justo. Class aptent taciti
+        sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc at mollis metus. Sed placerat placerat enim ac molestie. Fusce tempor viverra sapien, non sodales ex. Ut at eros volutpat, tincidunt arcu id, viverra ex. Nulla faucibus a arcu sit amet condimentum.
+        Aliquam facilisis vehicula turpis non vestibulum. Pellentesque euismod sapien sed purus ornare, id vulputate justo consectetur.
+        Sed bibendum eleifend sem, ac laoreet odio congue at. Nulla nunc nisi, mollis non varius nec, vehicula ac lorem. Suspendisse convallis lorem
+        eu arcu pellentesque lacinia in vitae turpis. Quisque fermentum semper ex id euismod. Aliquam posuere semper pharetra. Vivamus ex nibh, blandit non urna non, tempor vehicula massa. Mauris vel velit nec arcu gravida cursus.
+        Curabitur consectetur eget justo vel tristique. Nulla eu ornare erat. Cras lectus enim, mollis vitae metus vel, sollicitudin volutpat turpis. </p>
+        <?php
+        
+        ?>
+    </body>
+</html>
